9a78f52efe9932a2f232d8a29a44767498f4cc2a
Joseph Choi <jsphchoi@mit.edu>
2026-04-27 14:35:21 -0400

    create organized files

--- src/ExaModelsPEtab.jl
+++ src/ExaModelsPEtab.jl
@@ -1,5 +1,31 @@
+"""
+    ExaModelsPEtab
+
+(description)
+
+## Example Header
+
+(description)
+"""
 module ExaModelsPEtab
 
-# Write your package code here.
+    # Imports
+    import ExaModels
+    import PEtab # TODO trim dependencies, implement minimal parser
+
+    # Include functions
+    include("structs.jl")       # data structure for parameter estimation problem
+    include("constants.jl")     # get collocation equation constants
+    include("utils.jl")         # build helper functions
+    include("initialize.jl")    # get good initial conditions
+    include("variables.jl")     # create decision variables
+    include("collocation.jl")   # create collocation equality constraints
+    include("continuity.jl")    # create continuity equality constraints
+    include("objective.jl")     # create objective function
+
+    include("userfuncs.jl")        # user-end functions
+
+    # Exports
+    export petab_model
 
 end
